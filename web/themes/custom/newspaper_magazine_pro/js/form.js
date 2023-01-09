fc3dbf81e142206cc2e30fd904416deef767954f
Linzy <y.liashchuk@dev-branch.com>
2023-01-09 14:28:58 +0200

    #5811 Fixed display of block with infinite scrolling and rewrote templates to display this block, added styles for showing comments and breadcrumbs

--- web/themes/custom/newspaper_magazine_pro/js/form.js
+++ web/themes/custom/newspaper_magazine_pro/js/form.js
@@ -4,11 +4,21 @@
       this.ChangeText();
     },ChangeText: function () {
       jQuery("document").ready(function($){
+        console.log('aaa');
         let button = $('.comment-form .button');
         let title = jQuery('.comment-wrapper .title');
+        let url = jQuery('.js-form-type-url input');
+        let email = jQuery('.form-type-email input');
+        let name = jQuery('.form-type-textfield input');
+        let comment = jQuery('.form-type-textarea textarea');
 
         button.val('Post Comment');
         title.text('LEAVE A REPLY');
+
+        url.attr('placeholder', 'Website');
+        email.attr('placeholder', 'Email*');
+        name.attr('placeholder', 'Name*');
+        comment.attr('placeholder', 'Comment');
       });
     }
   };
